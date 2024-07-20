b3f3c9f7437e8c86d6f2055cc2e99774a32b9cbe
SLMEDHA <132287415+SLMEDHA@users.noreply.github.com>
2024-07-20 18:17:53 +0530

    Time: 3 ms (22.83%), Space: 7.3 MB (24.58%) - LeetHub

--- 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.cpp
+++ 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.cpp
@@ -3,7 +3,7 @@ public:
     int tribonacci(int n) {
         if(n==0)return 0;
         if(n==1||n==2)return 1;
-         int T0 = 0, T1 = 1, T2 = 1;
+        int T0 = 0, T1 = 1, T2 = 1;
         int Tn;
         
         for (int i = 3; i <= n; ++i) {
